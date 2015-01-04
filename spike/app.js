171a83486c672a9eec48e78e9f825c44571330d0
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-01-04 16:20:25 +0100

    Controllers and routing.

--- spike/app.js
+++ spike/app.js
@@ -1,1 +1,26 @@
-var chirpApp = angular.module('chirpApp', []);
+var chirpApp = angular.module('chirpApp', ['ngRoute']);
+
+chirpApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/post.html',
+            controller: 'postController'
+        })
+        .when('/post', {
+            templateUrl: 'pages/post.html',
+            controller: 'postController'
+        })
+        .when('/wall', {
+            templateUrl: 'pages/wall.html',
+            controller: 'wallController'
+        });
+});
+
+chirpApp.controller('postController', ['$scope', function($scope) {
+    console.log("post")
+}]);
+
+
+chirpApp.controller('wallController', ['$scope', function($scope) {
+    console.log("wall")
+}]);
