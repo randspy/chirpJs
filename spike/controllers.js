7a2a4b859db80ea331b35de5978b51ceb69505ab
Przemyslaw Koziel <mos.koziel@gmail.com>
2015-01-04 18:57:31 +0100

    Basic post read and write. End of the spike. Time to do some tdd.

--- spike/controllers.js
+++ spike/controllers.js
@@ -1,9 +1,31 @@
-chirpApp.controller('showPostsController', ['$scope', function($scope) {
-    console.log("showPostsController")
+chirpApp.service('chirpData', function() {
+
+    this.users = [];
+});
+
+chirpApp.controller('showPostsController', ['$scope', 'chirpData', function($scope, chirpData) {
+    for (var idx = 0; idx < chirpData.users.length; idx++){
+        if ($scope.username === chirpData.users[idx]["username"]){
+            if ($scope.posts == undefined)
+            {
+                $scope.posts = [];
+            }
+            $scope.posts.push(chirpData.users[idx]["value"]);
+        }
+    }
+
+    console.log($scope.posts);
 }]);
 
-chirpApp.controller('newPostController', ['$scope', function($scope) {
-    console.log("newPostController")
+chirpApp.controller('newPostController', ['$scope', 'chirpData', function($scope, chirpData) {
+
+    $scope.writeNewPost = function(username){
+        var newPostObj = {};
+        newPostObj['username'] = $scope.username;
+        newPostObj['value'] = $scope.newPost;
+        chirpData.users.push(newPostObj);
+        console.log(chirpData);
+    };
 }]);
 
 chirpApp.controller('followController', ['$scope', function($scope) {
